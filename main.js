4c4f7a081e9d920316bc661da28b63840ac0c8f9
Ida Casperson <ida_casperson@hotmail.com>
2023-11-08 14:34:38 +0100

    Updates buttons to action

--- main.js
+++ main.js
@@ -3,86 +3,39 @@ window.addEventListener("DOMContentLoaded", main);
 function main() {
   renderScene(); //Visar scen 1
 }
+
 function renderScene() {
   const text = document.getElementById("text");
-  const btn1 = document.getElementById("btn-1");
-  const btn2 = document.getElementById("btn-2");
-  const btn3 = document.getElementById("btn-3");
-  const btn4 = document.getElementById("btn-4");
-  const backgroundMusic = new Audio("music/fantasy.mp3");
-
-  backgroundMusic.volume = 0.5;
-  backgroundMusic.loop = true;
-  backgroundMusic.play();
+  const actionButtons = document.getElementById("action-buttons");
 
   const scene = scenes[activeSceneIndex]; // Nu visas scene 1 på webbläsaren, sätts ihop med scenes.js scenes
-  text.textContent = scene.text;
-  btn1.textContent = scene.button1.text;
-  btn2.textContent = scene.button2.text;
-  // CSS för bodyn
-  document.body.style.backgroundImage = scene.background;
-
-  //CSS för h2
-  text.textContent = scene.text;
-  text.classList.add("font");
-
-  //CSS för knapparna
-  btn1.textContent = scene.button1.text;
-  btn1.classList.add("col3", "btn-1");
-
-  btn2.textContent = scene.button2.text;
-  btn2.classList.add("col3", "btn-2");
-
-  // Döljer knapp 3 och knapp 4
-  btn3.style.display = "none";
-  btn4.style.display = "none";
-
-  // Gör så att knapp 3 och 4 visas bara på scen 1
-  if (activeSceneIndex === 1) {
-    btn3.textContent = scene.button3.text;
-    btn3.classList.add("col3", "btn-3");
-    btn3.style.display = "block";
-
-    btn4.textContent = scene.button4.text;
-    btn4.classList.add("col3", "btn-4");
-    btn4.style.display = "block";
-  } else {
-    btn3.style.display = "none";
-    btn4.style.display = "none";
+  // Lite musik genom scenerna
+  if (scene.startBackgroundMusic) {
+    const backgroundMusic = new Audio("music/harryPotter.mp3");
+    backgroundMusic.volume = 0.5;
+    backgroundMusic.loop = true;
+    backgroundMusic.play();
   }
-  // Döljer knapp 2 i scen 5 och 7
-  if (activeSceneIndex === 5 || activeSceneIndex === 7) {
-    btn2.style.display = "none";
-  } else {
-    btn2.style.display = "block";
+  document.body.style.backgroundImage = scene.background;
+  text.textContent = scene.text;
+
+  actionButtons.innerHTML = "";
+  for (const action of scene.actions) {
+    const btn = document.createElement("button");
+    btn.textContent = action.text;
+    btn.className = "btn";
+    // Nedan, så vill hamna på en ny scen med onClick, vi ska inte anropa utan systemet ska därav inga ()
+    // Nedan, Kallas anonym function
+    // Första knappen
+    btn.onclick = function () {
+      goToNextScene(action.activeSceneIndex);
+    };
+    actionButtons.append(btn);
   }
 
   if (scene.things) {
-    btn3.onclick = function () {
-      const inventoryList = document.getElementById("inventory-list");
-
-      scene.things.forEach((item) => {
-        const itemElement = document.createElement("ul");
-        itemElement.style.backgroundImage = item.url;
-        itemElement.className = "inventory-item";
-      });
-    };
-    btn4.onclick = function () {
-      inventoryList.appendChild(itemElement);
-    };
-  } else {
+    // Visa bilder på sidan (for loop)
   }
-
-  // Nedan, så vill hamna på en ny scen med onClick, vi ska inte anropa utan systemet ska därav inga ()
-  // Nedan, Kallas anonym function
-  // Första knappen
-  btn1.onclick = function () {
-    goToNextScene(scene.button1.activeSceneIndex);
-  };
-  //Andra knappen om man klickar på den
-  btn2.onclick = function () {
-    goToNextScene(scene.button2.activeSceneIndex);
-  };
 }
 
 // Function till goToNextScen
--- scenes.js
+++ scenes.js
@@ -6,17 +6,20 @@ const scenes = [
   {
     text: "Välkommen till Hogwarts äventyr. Vilken tur att du är här, Ron har blivit kidnappad av Lord Voldemort, kan du rädda honom?",
     background: 'url("pics/sorting-hat.jpeg")',
-    button1: {
-      text: "Ja det kan jag",
-      activeSceneIndex: 1,
-    },
-    button2: {
-      text: "Nej, jag vågar inte",
-      activeSceneIndex: 5,
-    },
+    actions: [
+      {
+        text: "Ja det kan jag",
+        activeSceneIndex: 1,
+      },
+      {
+        text: "Nej, jag vågar inte",
+        activeSceneIndex: 5,
+      },
+    ],
   },
   // Scen Hagrid - 1
   {
+    startBackgroundMusic: true,
     background: 'url("pics/hagrids-house.png")',
     text: "Det är jag som är Rubeus Hagrid! Jag är en halvjätte och arbetar som skogvaktare här på Hogwarts. Vilken tur att du har kommit, Ron är verkligen i fara! Behöver du något som kan hjälpa honom? Såg att det fanns en trollstav här någon stans. Om du ser trollstaven tar den med dig.",
     things: [
@@ -30,7 +33,7 @@ const scenes = [
       },
       {
         name: "Pumpa",
-        url: 'url("pics/pumpajpg")',
+        url: "pics/pumpa.jpg",
       },
       {
         name: "Sten",
@@ -41,92 +44,92 @@ const scenes = [
         url: 'url("pics/fjäder.jpg")',
       },
     ],
-    button1: {
-      text: "Gå till kartan",
-      activeSceneIndex: 2,
-    },
-    button2: {
-      text: "Gå tillbaka",
-      activeSceneIndex: 0,
-    },
-    button3: {
-      text: "Klicka här för lista av redskap",
-    },
-    button4: {
-      text: "Klicka här för att välja",
-    },
+    actions: [
+      {
+        text: "Gå till kartan",
+        activeSceneIndex: 2,
+      },
+      {
+        text: "Gå tillbaka",
+        activeSceneIndex: 0,
+      },
+    ],
   },
   //Scen - Karta - 2
   {
     background: 'url("pics/map3.jpg")',
     text: "Du har hittat kartan! Här kan du hitta vägen till Ron. Ser ut som att du kommer att få gå till den förbjudna skogen. Det är där Ron hålls fången. Bara du kan rädda honom nu!",
-    button1: {
-      text: "Gå till skogen",
-      activeSceneIndex: 3,
-    },
-    button2: {
-      text: "Gå tillbaka till Hagrid",
-      activeSceneIndex: 1,
-    },
+    actions: [
+      {
+        text: "Gå till skogen",
+        activeSceneIndex: 3,
+      },
+      {
+        text: "Gå tillbaka till Hagrid",
+        activeSceneIndex: 1,
+      },
+    ],
   }, // Scen - karta - 3
   {
     background: 'url("pics/map3.jpg")',
     text: "Du har hittat kartan! Här kan du hitta vägen till Ron. Ser ut som att du kommer att få gå till den förbjudna skogen. Det är där Ron hålls fången. Bara du kan rädda honom nu!",
-    button1: {
-      text: "Gå till skogen",
-      activeSceneIndex: 4,
-    },
-    button2: {
-      text: "Gå tillbaka till Hagrid",
-      activeSceneIndex: 1,
-    },
+    actions: [
+      {
+        text: "Gå till skogen",
+        activeSceneIndex: 4,
+      },
+      {
+        text: "Gå tillbaka till Hagrid",
+        activeSceneIndex: 1,
+      },
+    ],
   }, // Scen skogen 4
   {
     background: 'url("pics/car.png")',
     text: "Dags att möta Voldemort, kasta iväg en besvärjelse för att rädda Ron innan det är försent!",
-    button1: {
-      text: "Defendio",
-      activeSceneIndex: 7,
-    },
-    button2: {
-      text: "Kastar en sten",
-      activeSceneIndex: 6,
-    },
+    actions: [
+      {
+        text: "Defendio",
+        activeSceneIndex: 7,
+      },
+      {
+        text: "Kastar en sten",
+        activeSceneIndex: 6,
+      },
+    ],
   }, // Fail mission page - 5
   {
-    background: 'url("pics/roon.jpg")',
+    background: 'url("pics/sorting-hat.jpeg")',
     text: "Du är en mes, ingen äkta trollkarl eller häxa!",
-    button1: {
-      text: "Gå tillbaka",
-      activeSceneIndex: 0,
-    },
-    button2: {
-      text: "Error",
-      activeSceneIndex: 0,
-    },
+    actions: [
+      {
+        text: "Gå tillbaka",
+        activeSceneIndex: 0,
+      },
+    ],
   }, // Fail page 2 - 6
   {
-    background: 'url("pics/roon.jpg")',
+    background: 'url("pics/sadRon.jpg")',
     text: "Du kan inte kasta en sten mot Lord Voldemort ? Försök igen",
-    button1: {
-      text: "Försök igen",
-      activeSceneIndex: 4,
-    },
-    button2: {
-      text: "Gå tillbaka",
-      activeSceneIndex: 3,
-    },
+    actions: [
+      {
+        text: "Försök igen",
+        activeSceneIndex: 4,
+      },
+      {
+        text: "Gå tillbaka",
+        activeSceneIndex: 3,
+      },
+    ],
   }, // scen endGame - 7
   {
-    background: 'url("pics/celebration.jpg")',
+    background: 'url("pics/vinst.jpg")',
     text: "Tack så mycket för att du räddade mitt liv, jag är evigt tacksam!",
-    button1: {
-      text: "Tillbaka",
-      activeSceneIndex: 0,
-    },
-    button2: {
-      text: "Gå tillbaka",
-      activeSceneIndex: 4,
-    },
+    actions: [
+      {
+        text: "Tillbaka",
+        activeSceneIndex: 0,
+      },
+    ],
   },
 ];
