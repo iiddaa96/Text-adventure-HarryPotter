591ba8332184a3efbe76b3d807f91f984fed99a1
Ida Casperson <ida_casperson@hotmail.com>
2023-11-13 09:30:57 +0100

    update pics

--- scenes.js
+++ scenes.js
@@ -5,7 +5,7 @@ const scenes = [
   // Scen- Start - 0
   {
     text: "Välkommen till Hogwarts äventyr. Vilken tur att du är här, Ron har blivit kidnappad av Lord Voldemort, kan du rädda honom?",
-    background: 'url("pics/sorting-hat.jpeg")',
+    background: 'url("pics/slottet.jpg")',
     actions: [
       {
         text: "Ja det kan jag",
@@ -13,14 +13,14 @@ const scenes = [
       },
       {
         text: "Nej, jag vågar inte",
-        activeSceneIndex: 5,
+        activeSceneIndex: 4,
       },
     ],
   },
   // Scen Hagrid - 1
   {
     startBackgroundMusic: true,
-    background: 'url("pics/hagrids-house.png")',
+    background: 'url("pics/hagrid.jpg")',
     text: "Det är jag som är Rubeus Hagrid! Jag är en halvjätte och arbetar som skogvaktare här på Hogwarts. Vilken tur att du har kommit, Ron är verkligen i fara! Du behöver plocka med dig en trollstav och en hatt. Tyckte jag såg att det fanns en trollstav här någon stans, hmm..",
     things: [
       {
@@ -57,7 +57,7 @@ const scenes = [
   },
   //Scen - Karta - 2
   {
-    background: 'url("pics/map3.jpg")',
+    background: 'url("pics/map.jpg")',
     text: "Du har hittat kartan! Här kan du hitta vägen till Ron. Ser ut som att du kommer att få gå till den förbjudna skogen. Det är där Ron hålls fången. Bara du kan rädda honom nu!",
     actions: [
       {
@@ -69,37 +69,23 @@ const scenes = [
         activeSceneIndex: 1,
       },
     ],
-  }, // Scen - karta - 3
+  }, // Scen skogen - 3
   {
-    background: 'url("pics/map3.jpg")',
-    text: "Du har hittat kartan! Här kan du hitta vägen till Ron. Ser ut som att du kommer att få gå till den förbjudna skogen. Det är där Ron hålls fången. Bara du kan rädda honom nu!",
-    actions: [
-      {
-        text: "Gå till skogen",
-        activeSceneIndex: 4,
-      },
-      {
-        text: "Gå tillbaka till Hagrid",
-        activeSceneIndex: 1,
-      },
-    ],
-  }, // Scen skogen 4
-  {
-    background: 'url("pics/car.png")',
+    background: 'url("pics/skogen.jpg")',
     text: "Dags att möta Voldemort, kasta iväg en besvärjelse för att rädda Ron innan det är försent!",
     actions: [
       {
         text: "Defendio",
-        activeSceneIndex: 7,
+        activeSceneIndex: 6,
       },
       {
         text: "Kastar en sten",
-        activeSceneIndex: 6,
+        activeSceneIndex: 5,
       },
     ],
-  }, // Fail mission page - 5
+  }, // Fail mission page - 4
   {
-    background: 'url("pics/sorting-hat.jpeg")',
+    background: 'url("pics/slottet.jpg")',
     text: "Du är en mes, ingen äkta trollkarl eller häxa!",
     actions: [
       {
@@ -107,21 +93,21 @@ const scenes = [
         activeSceneIndex: 0,
       },
     ],
-  }, // Fail page 2 - 6
+  }, // scen Fail page  - 5
   {
     background: 'url("pics/sadRon.jpg")',
     text: "Du kan inte kasta en sten mot Lord Voldemort ? Försök igen",
     actions: [
       {
         text: "Försök igen",
-        activeSceneIndex: 4,
+        activeSceneIndex: 3,
       },
       {
         text: "Gå tillbaka",
-        activeSceneIndex: 3,
+        activeSceneIndex: 2,
       },
     ],
-  }, // scen endGame - 7
+  }, // scen endGame - 6
   {
     background: 'url("pics/vinst.jpg")',
     text: "Tack så mycket för att du räddade mitt liv, jag är evigt tacksam!",
